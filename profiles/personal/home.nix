1a18fb18b5b47e60b0d0d784e31f3f8e72298d1c
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-04-28 13:09:43 +0200

    (home): add automatic GC to nix

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -20,6 +20,11 @@
     settings = {
       experimental-features = "nix-command flakes";
     };
+    gc = {
+      automatic = true;
+      frequency = "weekly";
+      options = "--delete-generations 7d";
+    };
   };
 
   nixpkgs.config.allowUnfree = true; # nix-community/home-manager/#463
