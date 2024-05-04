ceba26d66ad7074d4ec44984a116228cbf280972
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-05-04 12:04:11 +0200

    (kdeconnect): add the tray.target workaround

--- user/kdeconnect.nix
+++ user/kdeconnect.nix
@@ -3,4 +3,13 @@
     enable = true;
     indicator = true;
   };
+
+  # Workaround for Failed to restart syncthingtray.service: Unit tray.target not found.
+  # Reference: https://github.com/nix-community/home-manager/issues/2064
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = ["graphical-session-pre.target"];
+    };
+  };
 }
