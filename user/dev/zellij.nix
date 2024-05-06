c0f4910b3f4f1315ca68a962ff5a2eaf09e3e0a3
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-05-06 21:41:30 +0200

    (zellij): set-up the Konsole profile

--- user/dev/zellij.nix
+++ user/dev/zellij.nix
@@ -1,6 +1,30 @@
-{...}: {
+{config, ...}: {
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
+  };
+
+  # NOTE Konsole splits the config in this weird way
+
+  # add the profile
+  home.file."konsolerc" = {
+    enable = true;
+    text = ''
+      [Desktop Entry]
+      DefaultProfile=Zellij.profile
+    '';
+    target = "${config.xdg.configHome}/konsolerc";
+  };
+
+  # define the profile
+  home.file."Zellij.profile" = {
+    enable = true;
+    text = ''
+      [General]
+      Command=zellij -l welcome
+      Name=Zellij
+      Parent=FALLBACK/
+    '';
+    target = "${config.xdg.dataHome}/konsole/Zellij.profile";
   };
 }
