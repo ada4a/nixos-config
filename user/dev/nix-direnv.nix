732c3c0f698fe7b4b524a2839b8e731c9b80bdb3
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-05-18 12:05:13 +0200

    (direnv): hide env diffs since they really clutter stdout
    Reference: https://direnv.net/man/direnv.toml.1.html#codehideenvdiffcode

--- user/dev/nix-direnv.nix
+++ user/dev/nix-direnv.nix
@@ -3,6 +3,9 @@
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
+    config = {
+      global.hide_env_diff = true;
+    };
   };
 
   programs.zsh.oh-my-zsh.plugins = ["direnv"];
