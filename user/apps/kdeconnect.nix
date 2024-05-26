bd79f2fafb998fd0b787ed6a1a1f4647154d99c2
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-05-26 15:54:46 +0200

    feat(home/kdeconnect): rm superfluous .desktop entries
    Inspiration:
    https://github.com/Misterio77/nix-config/blob/main/home/misterio/features/desktop/common/kdeconnect.nix#L12

--- user/apps/kdeconnect.nix
+++ user/apps/kdeconnect.nix
@@ -1,4 +1,24 @@
 {pkgs, ...}: {
+  # Hide all .desktop entries, except for org.kde.kdeconnect.settings
+  # https://github.com/Misterio77/nix-config/blob/main/home/misterio/features/desktop/common/kdeconnect.nix#L12
+  xdg.desktopEntries = {
+    "org.kde.kdeconnect.sms" = {
+      exec = "";
+      name = "KDE Connect SMS";
+      settings.NoDisplay = "true";
+    };
+    "org.kde.kdeconnect.nonplasma" = {
+      exec = "";
+      name = "KDE Connect Indicator";
+      settings.NoDisplay = "true";
+    };
+    "org.kde.kdeconnect.app" = {
+      exec = "";
+      name = "KDE Connect";
+      settings.NoDisplay = "true";
+    };
+  };
+
   services.kdeconnect = {
     enable = true;
     # use the Plasma 6 package
