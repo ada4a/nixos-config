6bc1550bba5982aed7285de22a66f2eea1e78399
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-05-26 16:15:22 +0200

    unsure(home/nix): rm most of the config, since it's covered by system config?

--- user/nix.nix
+++ user/nix.nix
@@ -1,13 +1,3 @@
-{pkgs, ...}: {
-  nix = {
-    package = pkgs.nix;
-    settings = {
-      experimental-features = "nix-command flakes";
-    };
-    gc = {
-      automatic = true;
-      frequency = "weekly";
-      options = "--delete-generations 7d";
-    };
-  };
+{...}: {
+  nix.gc.options = "--delete-generations 7d";
 }
