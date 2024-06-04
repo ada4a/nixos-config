a80ace3a3ded73bea093524420eb30d80e4a0603
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-06-04 23:55:10 +0200

    feat(home/wezterm): init

--- user/dev/default.nix
+++ user/dev/default.nix
@@ -9,6 +9,7 @@
     ./neovim.nix
     (import ./git.nix {inherit userSettings;})
     ./vscode.nix
+    ./wezterm
     # disable for now because of a bug
     # reference: https://github.com/zellij-org/zellij/issues/2925
     #./zellij.nix
