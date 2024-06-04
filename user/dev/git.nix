8e5617c75dfd2afef719ef44fdc846da9afb2728
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-06-04 13:25:17 +0200

    feat(home/git): add an alias for a log tree

--- user/dev/git.nix
+++ user/dev/git.nix
@@ -3,6 +3,9 @@
     enable = true;
     userName = userSettings.name;
     userEmail = userSettings.gmail;
+    aliases = {
+      "tree" = "git log --oneline --graph --color --all --decorate";
+    };
     extraConfig = {
       init.defaultBranch = "main";
       core.editor = "nvim";
