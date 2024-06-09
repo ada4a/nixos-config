adc8459a1fcc52e89126e8d587b24a91496d0ad2
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-06-09 15:11:24 +0200

    (flake/devShell): rm git since it's needed to access the devShell in the first place
    `nix develop` requires git for some reason

--- shell.nix
+++ shell.nix
@@ -2,9 +2,9 @@
   default = pkgs.mkShell {
     NIX_CONFIG = "extra-experimental-features = nix-command flakes ca-derivations";
     nativeBuildInputs = with pkgs; [
+      nh
       nix
       home-manager
-      git
     ];
   };
 }
