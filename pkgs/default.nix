580aee82420b6a02a41ba67b4fbaeb2334a4efa9
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-06-11 11:26:31 +0200

    feat(pkgs): add ddcci-plasmoid

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,2 +1,6 @@
-{...}: {
+{pkgs, ...}: let
+  inherit (pkgs) callPackage;
+in {
+  ddcci-plasmoid-backend = callPackage ./ddcci-plasmoid-backend {};
+  ddcci-plasmoid = callPackage ./ddcci-plasmoid {};
 }
