c974ac235cc5270f1809806fae32e2f8a5541697
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-06-22 19:59:06 +0200

    fix(home/thunderbird): add OAuth2 for Gmail using a hack

--- user/apps/thunderbird.nix
+++ user/apps/thunderbird.nix
@@ -6,7 +6,15 @@
     };
   };
   accounts.email.accounts = {
-    # "gmail".thunderbird.enable = true;
+    "gmail".thunderbird = {
+      enable = true;
+      # HACK to enable OAuth2
+      # https://github.com/nix-community/home-manager/issues/4988#issue-2113760749
+      settings = id: {
+        "mail.smtpserver.smtp_${id}.authMethod" = 10;
+        "mail.server.server_${id}.authMethod" = 10;
+      };
+    };
     "rmail".thunderbird.enable = true;
   };
 }
