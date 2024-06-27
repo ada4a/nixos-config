693eaf61f332ab88e52fd002747478aaa5a95139
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-06-27 19:27:55 +0200

    feat(nixos/steam): rm steam-run-native
    I don't think I actually need this

--- system/steam.nix
+++ system/steam.nix
@@ -1,7 +1,4 @@
 # Reference: https://linuxhint.com/how-to-instal-steam-on-nixos/
-{pkgs,...}: {
+{...}: {
   programs.steam.enable = true;
-  environment.systemPackages = with pkgs; [
-    steam-run-native
-  ];
 }
