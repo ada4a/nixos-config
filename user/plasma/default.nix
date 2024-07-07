84a8dcf1028b92eb135e53c072ac4576b82e9530
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-07 17:53:26 +0200

    feat(home/plasma): add panel widgets

--- user/plasma/default.nix
+++ user/plasma/default.nix
@@ -8,6 +8,28 @@
         location = "bottom";
         floating = true;
         height = 42;
+        widgets = [
+          "org.kde.plasma.kickoff"
+          "org.kde.plasma.showActivityManager"
+          {
+            name = "org.kde.plasma.icontasks";
+            config.General = {
+              launchers = [
+                "preferred://browser"
+                "applications:obsidian.desktop"
+                # FIXME this really should be preferred://terminal or something
+                "applications:org.wezfurlong.wezterm.desktop"
+                "preferred://filemanager"
+                # FIXME this really should be preferred://editor or something
+                "applications:nvim.desktop"
+              ];
+            };
+          }
+          "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+          "org.kde.plasma.showdesktop"
+        ];
       }
     ];
     workspace = {
