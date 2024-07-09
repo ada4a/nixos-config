47b78d3d70549a2c9058b21e0d98c54bc738ffdf
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-09 12:45:13 +0200

    feat(home/plasma): init application rules

--- user/plasma.nix
+++ user/plasma.nix
@@ -2,6 +2,7 @@
   programs.plasma = {
     enable = true;
     overrideConfig = true;
+
     configFile = {
       # TODO switch to the high-level input module once it lands
       # https://github.com/nix-community/plasma-manager/pull/123
@@ -53,6 +54,43 @@
         ];
       }
     ];
+
+    window-rules = [
+      {
+        description = "When opening a console, go to Activity \"Default\"";
+        match = {
+          window-class = "konsole";
+          window-types = ["normal"];
+        };
+        # apply = {
+        #   activity = <activity UUID>; # move to the activity with this UUID?
+        #   activityrule = 3; # apply on all Activities?
+        # };
+      }
+      {
+        description = "Share Firefox across Activities";
+        match = {
+          window-class = "firefox";
+          window-types = ["normal"];
+        };
+        # apply = {
+        #   activity = "00000000-0000-0000-0000-000000000000" (move to all activities? i.e. share across activities);
+        #   activityrule = 3;
+        # };
+      }
+      {
+        description = "Share Obsidian across Activities";
+        match = {
+          window-class = "obsidian";
+          window-types = ["normal"];
+        };
+        # apply = {
+        #   activity = "00000000-0000-0000-0000-000000000000" (move to all activities? i.e. share across activities);
+        #   activityrule = 3;
+        # };
+      }
+    ];
+
     workspace = {
       clickItemTo = "select";
     };
