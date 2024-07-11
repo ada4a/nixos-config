e25fa2b866d4fec7c10a8282a57d4accc86c8228
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-11 20:55:11 +0200

    feat(home/zsh): set up completion with fzf-tab

--- user/dev/zsh.nix
+++ user/dev/zsh.nix
@@ -28,5 +28,14 @@
         map (x: "ohmyzsh/ohmyzsh path:plugins/${x}") ["git" "rust" "sudo" "sudo" "systemd" "zoxide"]
         ++ ["Aloxaf/fzf-tab"];
     };
+    initExtra = ''
+      # Completion styling
+      # Stolen from: https://github.com/dreamsofautonomy/zensh/blob/0ef5b9384f22923c73ae521d5d74bebb7b5791f5/.zshrc#L70-L75
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' list-colors "$${(s.:.)LS_COLORS}"
+      zstyle ':completion:*' menu no
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+      zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+    '';
   };
 }
