11b4d3096f14bf98d39b6f47b7f421b72f5f899e
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-11 15:43:08 +0200

    refactor(home/zellij): use plasma-manager to set-up Konsole appropriately
    don't you just love plasma-manager
    
    Btw: no need for the `config.programs.plasma.enable` business since we
    add the set-up to `programs.plasma` directly, and therefore it won't get
    activated unless `programs.plasma.enable = true` anyway

--- user/dev/zellij/default.nix
+++ user/dev/zellij/default.nix
@@ -1,36 +1,13 @@
-{
-  config,
-  lib,
-  ...
-}: {
+{...}: {
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
   };
 
-  home.file = lib.mkIf config.programs.plasma.enable {
-    # NOTE Konsole splits the config in this weird way
-
-    # add the profile
-    "konsolerc" = {
-      enable = true;
-      text = ''
-        [Desktop Entry]
-        DefaultProfile=Zellij.profile
-      '';
-      target = "${config.xdg.configHome}/konsolerc";
-    };
-
-    # define the profile
-    "Zellij.profile" = {
-      enable = true;
-      text = ''
-        [General]
-        Command=zellij -l welcome
-        Name=Zellij
-        Parent=FALLBACK/
-      '';
-      target = "${config.xdg.dataHome}/konsole/Zellij.profile";
+  programs.konsole = {
+    defaultProfile = "Zellij";
+    profiles."Zellij" = {
+      command = "zellij -l welcome";
     };
   };
 }
