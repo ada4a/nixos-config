04b57a1c6d2d3122b83d36e99437d2efc285ff80
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-11 19:02:49 +0200

    feat(home/zellij): only enable the rest of the config if Zellij is in use

--- user/dev/zellij/default.nix
+++ user/dev/zellij/default.nix
@@ -1,10 +1,14 @@
-{...}: {
+{
+  lib,
+  config,
+  ...
+}: {
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
   };
 
-  programs.konsole = {
+  programs.konsole = lib.mkIf config.programs.zellij.enable {
     defaultProfile = "Zellij";
     profiles."Zellij" = {
       command = "zellij -l welcome";
