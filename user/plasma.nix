a449c4942691a583c28733c7c08428678da8c4ad
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-18 12:40:59 +0200

    feat(home/plasma): TIL there is a shortcut for this!

--- user/plasma.nix
+++ user/plasma.nix
@@ -41,6 +41,10 @@ in {
 
     shortcuts = {
       "kwin"."Window Maximize" = ["Meta+Up"];
+      # normally it's <Right> and <Left>, respectively
+      # but in my set-up the monitors is one-above-the-other, so remap accordingly
+      "kwin"."Window to Next Screen" = ["Meta+Alt+Up"];
+      "kwin"."Window to Previous Screen" = ["Meta+Alt+Down"];
     };
 
     panels = [
