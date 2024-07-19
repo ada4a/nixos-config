d59f869810c713fc75f547469f74997487a3dd51
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-19 17:51:28 +0200

    feat(home/nix-direnv): fix zsh integration
    1) don't use the OMZ plugin - since enableZshIntegration does the same
    things
    2) don't enableZshIntegration explicitely since it's enabled by default

--- user/dev/nix-direnv.nix
+++ user/dev/nix-direnv.nix
@@ -1,26 +1,22 @@
 {...}: {
   programs.direnv = {
     enable = true;
-    enableZshIntegration = true;
     nix-direnv.enable = true;
     config = {
       global.hide_env_diff = true;
     };
   };
 
-  programs.zsh = {
-    oh-my-zsh.plugins = ["direnv"];
-    # Reference: https://github.com/nix-community/nix-direnv/wiki/Shell-integration
-    initExtra = ''
-      flakify() {
-          if [ ! -e flake.nix ]; then
-              nix flake new -t github:ugur-a/nixos-config#nix-direnv .
-          elif [ ! -e .envrc ]; then
-              echo "use flake" > .envrc
-              direnv allow
-          fi
-          ''${EDITOR:-vim} flake.nix
-      }
-    '';
-  };
+  # Reference: https://github.com/nix-community/nix-direnv/wiki/Shell-integration
+  programs.zsh.initExtra = ''
+    flakify() {
+        if [ ! -e flake.nix ]; then
+            nix flake new -t github:ugur-a/nixos-config#nix-direnv .
+        elif [ ! -e .envrc ]; then
+            echo "use flake" > .envrc
+            direnv allow
+        fi
+        ''${EDITOR:-vim} flake.nix
+    }
+  '';
 }
