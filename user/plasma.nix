9df6310e678d1576cc020d06a20b23b5fd9cb26f
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-19 08:47:21 +0200

    feat(home/plasma): open System Settings with Meta+,

--- user/plasma.nix
+++ user/plasma.nix
@@ -49,6 +49,7 @@ in {
       # but in my set-up the monitors is one-above-the-other, so remap accordingly
       "kwin"."Window to Next Screen" = ["Meta+Alt+Up"];
       "kwin"."Window to Previous Screen" = ["Meta+Alt+Down"];
+      "services/systemsettings.desktop"."_launch" = ["Meta+,"];
     };
 
     panels = [
