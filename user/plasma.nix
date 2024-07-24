030d78cacb92206ca6b4c7cc87fe441a7275c523
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-24 15:31:32 +0200

    style(home/plasma/shortcuts): don't unnecessarily quote option names

--- user/plasma.nix
+++ user/plasma.nix
@@ -57,11 +57,11 @@ in {
     };
 
     shortcuts = {
-      "kwin"."Window Maximize" = ["Meta+Up"];
+      kwin."Window Maximize" = ["Meta+Up"];
       # normally it's <Right> and <Left>, respectively
       # but in my set-up the monitors is one-above-the-other, so remap accordingly
-      "kwin"."Window to Next Screen" = ["Meta+Alt+Up"];
-      "kwin"."Window to Previous Screen" = ["Meta+Alt+Down"];
+      kwin."Window to Next Screen" = ["Meta+Alt+Up"];
+      kwin."Window to Previous Screen" = ["Meta+Alt+Down"];
       "services/systemsettings.desktop"."_launch" = ["Meta+,"];
     };
 
