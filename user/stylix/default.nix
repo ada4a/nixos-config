4a11ab6ac26d307b204fe8f9479749ef3fc63758
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-25 19:54:12 +0200

    feat(home/stylix): enable autoEnable

--- user/stylix/default.nix
+++ user/stylix/default.nix
@@ -14,8 +14,10 @@
 in {
   stylix = {
     enable = true;
+    autoEnable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
     image = ./dummy.jpg;
+
     fonts = {
       monospace = fontToNameAndPackage userSettings.fonts.mono;
     };
