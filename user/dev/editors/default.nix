c75a4edc22a77bd67266f8ab80f39bb67bbdabc5
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-28 16:06:51 +0200

    feat(home/editors): install flake.nix's default formatter
    to use it as the default formatter for Nix

--- user/dev/editors/default.nix
+++ user/dev/editors/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  systemSettings,
+  outputs,
+  ...
+}:
 {
   imports = [
     ./helix.nix
@@ -6,22 +11,23 @@
     ./vscode.nix
   ];
 
-  home.packages = with pkgs; [
-    # LSPs
-    nixd
-    lua-language-server
-    rust-analyzer
+  home.packages =
+    (with pkgs; [
+      # LSPs
+      nixd
+      lua-language-server
+      rust-analyzer
 
-    # Debug Adapters (DAPs)
-    lldb # vscode-lldb
+      # Debug Adapters (DAPs)
+      lldb # vscode-lldb
 
-    # Formatters
-    # vimPlugins.vim-clang-format # conflicts with gruvbox-nvim & lazygit-nvim for some reason?
-    clang-tools # c(++), js(on)
-    alejandra
-    rustfmt
-    stylua
-    taplo # toml
-    # xmlformat
-  ];
+      # Formatters
+      # vimPlugins.vim-clang-format # conflicts with gruvbox-nvim & lazygit-nvim for some reason?
+      clang-tools # c(++), js(on)
+      rustfmt
+      stylua
+      taplo # toml
+      # xmlformat
+    ])
+    ++ [ outputs.formatter.${systemSettings.system} ];
 }
