1304659b7992c75147502385b01404ff86b420ce
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-08-02 19:00:10 +0200

    refactor(home/plasma/input): use the new high-level module

---
+++ user/plasma/input.nix
@@ -0,0 +1,15 @@
+{
+  programs.plasma.input = {
+    touchpads = [
+      {
+        enable = true;
+        vendorId = "04F3";
+        productId = "3109";
+        name = "ELAN2204:00 04F3:3109 Touchpad";
+        naturalScroll = true;
+        tapToClick = true;
+        pointerSpeed = 0.2;
+      }
+    ];
+  };
+}
