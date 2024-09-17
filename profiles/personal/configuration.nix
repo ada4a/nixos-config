667840d6ccf65aaeeb3a60e4beae06b17f55bc8b
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-09-17 19:30:27 +0200

    feat(nixos/i18n): don't set supportedLocales manually
    it already includes defaultLocale by default
    
    ref
    https://github.com/NixOS/nixpkgs/blob/99dc8785f6a0adac95f5e2ab05cc2e1bf666d172/nixos/modules/config/i18n.nix#L53-L60

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -30,7 +30,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = systemSettings.locale;
-  i18n.supportedLocales = [ "${systemSettings.locale}/UTF-8" ]; # make sure the locale is installed
 
   # Wayland
   security.polkit.enable = true;
