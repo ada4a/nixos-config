70b75a51ecb1baccee1240ea80a62ae8083d3fd1
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-09-17 19:09:26 +0200

    feat(nixos/nix): add some more settings
    not documented in search.nixos.org, see man nix.conf(5)

--- system/nix.nix
+++ system/nix.nix
@@ -7,12 +7,14 @@
   };
 
   nix.settings = {
+    auto-optimise-store = true;
     extra-experimental-features = [
       "nix-command"
       "flakes"
     ];
     # Opinionated: disable global registry^[1]
     flake-registry = "";
+    use-xdg-base-directories = true;
   };
 }
 # [1]: https://github.com/Misterio77/nix-starter-configs/blob/main/standard/nixos/configuration.nix#L52
