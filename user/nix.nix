e8db4de4ed187d8247fd7b7b6ebe3c3d74d8b72a
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-09-18 10:47:19 +0200

    fix(home/nix): enable use-xdg-directories so that home-manager moves .nix-profile

--- user/nix.nix
+++ user/nix.nix
@@ -1,1 +1,9 @@
-{ nix.gc.options = "--delete-generations 7d"; }
+{ pkgs, ... }:
+{
+  # required for the next line to work
+  nix.package = pkgs.nixFlakes;
+  # also set this here, since home-manager doesn't see the system config
+  # and doesn't move .nix-profile etc.
+  nix.settings.use-xdg-base-directories = true;
+  nix.gc.options = "--delete-generations 7d";
+}
