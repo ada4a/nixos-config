581aaf17fdfb866542da10e9f00ad08011146a9f
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-03 21:52:11 +0200

    feat(home/rust): make config from AOC2022 global

--- user/dev/rust.nix
+++ user/dev/rust.nix
@@ -19,6 +19,19 @@
     enable = true;
     settings = {
       build.target-dir = "${config.home.sessionVariables.CARGO_HOME}/target";
+      profile.release.debug = true;
+      lints.clippy = {
+        pedantic = {
+          level = "warn";
+          priority = -1;
+        };
+        cast_sign_loss = "allow";
+        cast_possible_truncation = "allow";
+        cast_possible_wrap = "allow";
+        missing_panics_doc = "allow";
+        missing_errors_doc = "allow";
+        must_use_candidate = "allow";
+      };
     };
   };
 }
