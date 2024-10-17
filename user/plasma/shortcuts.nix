c9eb61536a9190f1850159e1abaa70a6dce162e2
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-17 11:53:35 +0200

    fix(home/plasma/shortcuts): all this time I though the shortcuts weren't working!
    turns out I just had `Alt` instead of `Shift`

--- user/plasma/shortcuts.nix
+++ user/plasma/shortcuts.nix
@@ -3,8 +3,8 @@
     kwin."Window Maximize" = [ "Meta+Up" ];
     # normally it's <Right> and <Left>, respectively
     # but in my set-up the monitors is one-above-the-other, so remap accordingly
-    kwin."Window to Next Screen" = [ "Meta+Alt+Up" ];
-    kwin."Window to Previous Screen" = [ "Meta+Alt+Down" ];
+    kwin."Window to Next Screen" = [ "Meta+Shift+Up" ];
+    kwin."Window to Previous Screen" = [ "Meta+Shift+Down" ];
     "services/systemsettings.desktop"."_launch" = [ "Meta+," ];
   };
 }
