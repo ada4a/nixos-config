cf3166ac4d0afa274f1c0643fff1acf1d029fc00
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-17 13:28:53 +0200

    style(home/plasma/shortcuts): don't use lists when only one shortcut set

--- user/plasma/shortcuts.nix
+++ user/plasma/shortcuts.nix
@@ -1,10 +1,10 @@
 {
   programs.plasma.shortcuts = {
-    kwin."Window Maximize" = [ "Meta+Up" ];
+    kwin."Window Maximize" = "Meta+Up";
     # normally it's <Right> and <Left>, respectively
     # but in my set-up the monitors is one-above-the-other, so remap accordingly
-    kwin."Window to Next Screen" = [ "Meta+Shift+Up" ];
-    kwin."Window to Previous Screen" = [ "Meta+Shift+Down" ];
+    kwin."Window to Next Screen" = "Meta+Shift+Up";
+    kwin."Window to Previous Screen" = "Meta+Shift+Down";
     "services/systemsettings.desktop"."_launch" = "Meta+,";
   };
 }
