7736d862b361ea88ba62c9c98935e1d0721bc79f
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-24 12:32:53 +0200

    feat(home/neovim): re-add nvim-treesitter grammars
    I'll disable them in LazyVim instead

--- user/dev/editors/neovim.nix
+++ user/dev/editors/neovim.nix
@@ -1,8 +1,22 @@
-{ lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+    plugins = [
+      (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+        p.javascript
+        p.lua
+        p.markdown
+        p.nix
+        p.rust
+      ]))
+    ];
     viAlias = true;
     vimAlias = true;
   };
