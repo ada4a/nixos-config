22abbe1068eb2ea3baa172e4ca77a206656b3f02
Ada Alakbarova <ada.alakbarova@proton.me>
2025-01-16 09:46:10 +0100

    Revert "feat(home/direnv): put all `.direnv`s under one cache directory"
    This reverts commit 950cd1e675d2e1fe812f1b8edd549553460f2ca5.
    
    Losing the cache every time I move the directory was not exactly ideal

--- user/dev/nix-direnv.nix
+++ user/dev/nix-direnv.nix
@@ -11,20 +11,6 @@
         warn_timeout = "0s";
       };
     };
-    # put all `.direnv`s in one location
-    # https://github.com/direnv/direnv/wiki/Customizing-cache-location#human-readable-directories
-    stdlib = ''
-      : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
-      declare -A direnv_layout_dirs
-      direnv_layout_dir() {
-          local hash path
-          echo "''${direnv_layout_dirs[$PWD]:=$(
-              hash="$(sha1sum - <<< "$PWD" | head -c40)"
-              path="''${PWD//[^a-zA-Z0-9]/-}"
-              echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
-          )}"
-      }
-    '';
   };
 
   # Reference: https://github.com/nix-community/nix-direnv/wiki/Shell-integration
