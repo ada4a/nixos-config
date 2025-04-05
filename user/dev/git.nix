66215a8f863136734be2c97d5f521a2bfcd711f9
Ada Alakbarova <ada.alakbarova@proton.me>
2025-04-05 11:07:33 +0200

    feat(home/git): add stuff from Bits and Booze Ep. 14

--- user/dev/git.nix
+++ user/dev/git.nix
@@ -10,12 +10,41 @@
       "alias" = "config --get-regexp ^alias\.";
     };
     extraConfig = {
+      branch.sort = "-commiterdate";
+      column.ui = "auto";
+      commit.verbose = true;
       core.editor = "nvim";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      help.autocorrect = "prompt";
       init.defaultBranch = "main";
       merge.conflictstyle = "diff3";
       merge.tool = "meld";
-      push.autoSetupRemote = true;
-      rerere.enabled = true;
+      pull.rebase = true;
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      tag.sort = "version:refname";
     };
     lfs.enable = true;
   };
