00e3df73b1cdbcc2e838b4f98b320b7fc23855ac
Ada Alakbarova <ada.alakbarova@proton.me>
2025-06-25 00:19:00 +0200

    feat(home/nvim): add personal dictionary

--- user/dev/editors/neovim.nix
+++ user/dev/editors/neovim.nix
@@ -20,6 +20,17 @@
     viAlias = true;
     vimAlias = true;
   };
+
+  # add words to the personal dictionary
+  xdg.configFile."nvim/spell/en.utf-8.add" = {
+    enable = config.programs.neovim.enable;
+    text = ''
+      Mergiraf
+      Mergiraf's
+      Jujutsu
+    '';
+  };
+
   programs.zsh.shellAliases = lib.mkIf config.programs.neovim.enable {
     "NVIM_APPNAME" = "lazyvim";
   };
