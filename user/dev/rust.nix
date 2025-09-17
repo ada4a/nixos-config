cc6e4a8335b6ab19a9baaa5ce334e3a57c43f601
Ada Alakbarova <ada.alakbarova@proton.me>
2025-09-17 17:19:32 +0200

    feat(home/nvim): add clean configuration for rustc hacking

--- user/dev/rust.nix
+++ user/dev/rust.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   config,
   ...
 }:
@@ -18,5 +19,10 @@
 
   home.sessionVariables = {
     CARGO_HOME = "${config.xdg.cacheHome}/cargo";
+  };
+
+  programs.zsh.shellAliases = lib.mkIf config.programs.neovim.enable {
+    "nvimr" = "NVIM_APPNAME=nvim-rustc nvim";
+    "nr" = "NVIM_APPNAME=nvim-rustc nvim";
   };
 }
