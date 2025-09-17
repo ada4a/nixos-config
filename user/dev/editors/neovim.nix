21382f845cc0ef43162826057229178229536731
Ada Alakbarova <ada.alakbarova@proton.me>
2025-09-17 19:52:45 +0200

    feat(home/nvim): add `cfg`

--- user/dev/editors/neovim.nix
+++ user/dev/editors/neovim.nix
@@ -3,6 +3,9 @@
   config,
   ...
 }:
+let
+  cfg = config.programs.neovim;
+in
 {
   programs.neovim = {
     enable = true;
@@ -13,7 +16,7 @@
 
   # add words to the personal dictionary
   xdg.configFile."nvim/spell/en.utf-8.add" = {
-    enable = config.programs.neovim.enable;
+    enable = cfg.enable;
     text = ''
       Mergiraf
       Mergiraf's
@@ -21,7 +24,7 @@
     '';
   };
 
-  programs.zsh.shellAliases = lib.mkIf config.programs.neovim.enable {
+  programs.zsh.shellAliases = lib.mkIf cfg.enable {
     "NVIM_APPNAME" = "lazyvim";
   };
 
