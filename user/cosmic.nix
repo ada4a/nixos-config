6ed3081dcc9798cb766920608a36d3fab570fca4
Ada Alakbarova <ada.alakbarova@proton.me>
2025-11-03 17:17:41 +0100

    feat(cosmic): `dark`/`lite` commands

--- user/cosmic.nix
+++ user/cosmic.nix
@@ -1,4 +1,17 @@
+{ lib, systemSettings, ... }:
+let
+  cfg.enable = systemSettings.de == "cosmic";
+
+  setTheme = is_dark: "echo ${is_dark} > ~/.config/cosmic/com.system76.CosmicTheme.Mode/v1/is_dark";
+  setDarkTheme = setTheme "true";
+  setLightTheme = setTheme "false";
+in
 {
   home.packages = [
   ];
+
+  programs.zsh.shellAliases = lib.mkIf cfg.enable {
+    lite = setLightTheme;
+    dark = setDarkTheme;
+  };
 }
