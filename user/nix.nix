1d8b51a06cb5f6f63a9f8962ca4c64cfe2cfaa97
Ada Alakbarova <ada.alakbarova@proton.me>
2025-12-27 21:21:50 +0100

    feat(home/nix): add `n#` as a shortcut for `nixpkgs#`

--- user/nix.nix
+++ user/nix.nix
@@ -1,9 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
   # required for the next line to work
-  nix.package = pkgs.lixPackageSets.stable.lix;
-  # also set this here, since home-manager doesn't see the system config
-  # and doesn't move .nix-profile etc.
-  nix.settings.use-xdg-base-directories = true;
-  nix.gc.options = "--delete-generations 7d";
+  nix = {
+    package = pkgs.lixPackageSets.stable.lix;
+    # also set this here, since home-manager doesn't see the system config
+    # and doesn't move .nix-profile etc.
+    settings.use-xdg-base-directories = true;
+    gc.options = "--delete-generations 7d";
+    registry = {
+      # shortcut: `nix run n#foo` instead of `nix run nixpkgs#foo`
+      n = {
+        from = {
+          id = "n";
+          type = "indirect";
+        };
+        flake = inputs.nixpkgs;
+      };
+    };
+  };
 }
