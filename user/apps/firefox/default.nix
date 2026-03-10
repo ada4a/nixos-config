de70205ee7b49ef203139726017aedeff9eb6241
Ada Alakbarova <ada.alakbarova@proton.me>
2026-03-10 15:39:17 +0100

    feat(home/firefox): add a separate profile for Ferrous stuff
    1password nagging me constantly is pretty annoying

--- user/apps/firefox/default.nix
+++ user/apps/firefox/default.nix
@@ -12,42 +12,61 @@ in
   imports = [ ./engines.nix ];
 
   programs.firefox.enable = true;
-  programs.firefox.profiles."${userSettings.username}" = {
-    isDefault = true;
-    search = {
-      force = true;
-      default = "ddg";
-      privateDefault = "ddg";
-    };
-    settings = {
-      # kill it with fire
-      browser.ml.enable = false;
-      browser.ml.chat = {
-        enabled = false;
-        menu = false;
-        page.footerBadge = false;
-        page.menuBade = false;
-      };
-      browser.search.visualSearch.featureGate = false;
-      browser.tabs.groups.smart.enabled = false;
-      extensions.ml.enabled = false;
+  programs.firefox.profiles =
+    let
+      common = {
+        search = {
+          force = true;
+          default = "ddg";
+          privateDefault = "ddg";
+        };
+        settings = {
+          # kill it with fire
+          browser.ml.enable = false;
+          browser.ml.chat = {
+            enabled = false;
+            menu = false;
+            page.footerBadge = false;
+            page.menuBade = false;
+          };
+          browser.search.visualSearch.featureGate = false;
+          browser.tabs.groups.smart.enabled = false;
+          extensions.ml.enabled = false;
 
-      "widget.use-xdg-desktop-portal.file-picker" = "1";
+          "widget.use-xdg-desktop-portal.file-picker" = "1";
+        };
+        extensions.packages = with firefox-addons; [
+          bitwarden
+          british-english-dictionary-2
+          clearurls
+          darkreader
+          dictionary-german
+          duckduckgo-privacy-essentials
+          indie-wiki-buddy
+          return-youtube-dislikes
+          simple-tab-groups
+          ublock-origin
+          whowrotethat
+        ];
+      };
+    in
+    {
+      "${userSettings.username}" = lib.mkMerge [
+        common
+        {
+          id = 0; # implies `isDefault = true;`
+          extensions.packages = lib.optionals (
+            systemSettings.de == "plasma"
+          ) firefox-addons.plasma-integration;
+        }
+      ];
+      # should ideally have been in `ferrous.nix`, but then I wouldn't be able to access `common`
+      ferrous = lib.mkMerge [
+        common
+        {
+          id = 1;
+          extensions.packages = with firefox-addons; [ onepassword-password-manager ];
+        }
+      ];
     };
-    extensions.packages =
-      (with firefox-addons; [
-        bitwarden
-        british-english-dictionary-2
-        clearurls
-        darkreader
-        dictionary-german
-        duckduckgo-privacy-essentials
-        indie-wiki-buddy
-        return-youtube-dislikes
-        simple-tab-groups
-        ublock-origin
-        whowrotethat
-      ])
-      ++ lib.optionals (systemSettings.de == "plasma") firefox-addons.plasma-integration;
-  };
 }
