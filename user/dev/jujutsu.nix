e04b1aec17da86021af33d715b7794c216ce80ad
Ada Alakbarova <ada.alakbarova@proton.me>
2026-03-24 10:41:20 +0100

    feat(home/jj): show diff in `jj desc`

--- user/dev/jujutsu.nix
+++ user/dev/jujutsu.nix
@@ -23,6 +23,15 @@
       backend = "ssh";
       key = "~/.ssh/id_ed25519.pub";
     };
+    # show diff in `jj desc`
+    # see https://docs.jj-vcs.dev/v0.39.0/config/#default-description
+    templates.draft_commit_description = ''
+      concat(
+        builtin_draft_commit_description,
+        "\nJJ: ignore-rest\n",
+        diff.git(),
+      )
+    '';
     ui = {
       default-command = "status";
       diff-editor = ":builtin";
