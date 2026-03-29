eeccdffd8268f21cfa786dff5ddf5fbf775b4274
Ada Alakbarova <ada.alakbarova@proton.me>
2026-03-29 15:18:31 +0200

    fix(home/nix): get rid of the stupid nix xdg warnings
    I hope I won't see
    ```
    warning: Ignoring the client-specified setting 'use-xdg-base-directories', because it is a restricted setting and you are not a trusted user
    ```
    in my life ever again

--- user/nix.nix
+++ user/nix.nix
@@ -3,9 +3,8 @@
   # required for the next line to work
   nix = {
     package = pkgs.lixPackageSets.stable.lix;
-    # also set this here, since home-manager doesn't see the system config
-    # and doesn't move .nix-profile etc.
-    settings.use-xdg-base-directories = true;
+    # nix is set to use xdg by the system-level config, so assume that here
+    assumeXdg = true;
     gc.options = "--delete-generations 7d";
     registry = {
       # shortcut: `nix run n#foo` instead of `nix run nixpkgs#foo`
